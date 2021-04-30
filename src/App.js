acc6cfc07bfa7b23448fbe326664d8ae8d5b63a7
amol <amol>
2021-04-30 23:42:21 +0530

    appjs file changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
+    
     <div className="App">
+        <ShoppingList/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+      
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +22,91 @@ function App() {
           Learn React
         </a>
       </header>
+<Board/>
     </div>
   );
 }
 
-export default App;
+export default App
+class ShoppingList extends React.Component
+{
+ render()
+  {
+    return(
+    <div className="Shoppng-List">
+   <h1>Shopping List for {this.props.name}</h1>
+   <ul>
+     <li>Instagram---social media app</li>
+     <li>Whatsapp</li>
+     <li>Facebook</li>
+     <li>Facebook</li>
+     <li>Facebook</li>
+     <li>Facebook</li>
+     <li>Facebook</li>
+   </ul>
+    </div>
+    );
+  }
+}
+
+
+class Board extends React.Component
+{
+constructor(props)
+{
+  super(props);
+  this.state={
+    Squares:Array(9).fill(null),
+  }
+}
+handleClick(i)
+{
+  const squares=this.state.Squares.slice();
+  squares[i]='X';
+  this.setState({Squares:squares});
+
+}
+
+  renderSquare(i)
+{
+  return <Square value={this.state.Squares[i]}
+  onClick={()=>this.handleClick(i)}/>;
+}
+render()
+{
+  const status="next player x";
+  return(
+    
+    <div >
+      <div className="status">{status}</div>
+    <div className="board-row">
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
+      {this.renderSquare(8)}
+    </div>
+    </div>
+  )
+}
+}
+
+class Square extends React.Component
+{
+
+  render() {
+  return (
+    <button className="square" onClick={()=>this.props.onClick()}>
+      {this.props.value}
+    </button>
+  )
+  }
+}
