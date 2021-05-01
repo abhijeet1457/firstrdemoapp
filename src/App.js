0abd8879ba2b4deb08f330fd48f6ad41d64eb6af
amol <amol>
2021-05-01 23:24:25 +0530

    square dynamc method added

--- src/App.js
+++ src/App.js
@@ -57,13 +57,21 @@ constructor(props)
   super(props);
   this.state={
     Squares:Array(9).fill(null),
-  }
+    xIsNext:true,
+  };
 }
 handleClick(i)
 {
-  const squares=this.state.Squares.slice();
-  squares[i]='X';
-  this.setState({Squares:squares});
+  const squares = this.state.Squares.slice();
+  if (calculateWinner(squares) || squares[i]) {
+    return;
+  }
+  squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+  this.setState({
+    Squares:squares,
+  xIsNext:!this.state.xIsNext,
+  });
 
 }
 
@@ -72,41 +80,67 @@ handleClick(i)
   return <Square value={this.state.Squares[i]}
   onClick={()=>this.handleClick(i)}/>;
 }
-render()
-{
-  const status="next player x";
-  return(
-    
-    <div >
-      <div className="status">{status}</div>
-    <div className="board-row">
-      {this.renderSquare(0)}
-      {this.renderSquare(1)}
-      {this.renderSquare(2)}
-    </div>
-    <div className="board-row">
-      {this.renderSquare(3)}
-      {this.renderSquare(4)}
-      {this.renderSquare(5)}
-    </div>
-    <div className="board-row">
-      {this.renderSquare(6)}
-      {this.renderSquare(7)}
-      {this.renderSquare(8)}
-    </div>
-    </div>
-  )
-}
-}
+render() {
+  const winner = calculateWinner(this.state.Squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+  }
 
-class Square extends React.Component
-{
-
-  render() {
   return (
-    <button className="square" onClick={()=>this.props.onClick()}>
-      {this.props.value}
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+}
+
+
+function Square(props)
+{
+  return(
+    <button className="square" onClick={props.onClick}>
+      {props.value}
     </button>
   )
+  
+}
+
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
 }
